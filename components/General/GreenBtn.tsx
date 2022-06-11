884f69dd9c8ab6e356aab278c1978870b53d7893
stonelijah21 <80172565+stonelijah21@users.noreply.github.com>
2022-06-11 18:22:09 +0100

    ..

--- components/General/GreenBtn.tsx
+++ components/General/GreenBtn.tsx
@@ -9,7 +9,7 @@ type Props = {
 function GreenBtn({ children }: Props) {
   return (
     <div>
-      <Button variant="contained" color="success">
+      <Button variant="contained" className={styles.btnColor}>
         <Typography className={styles.text}>{children}</Typography>
       </Button>
     </div>
